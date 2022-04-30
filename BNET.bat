95331f4d877a08889b0f2faf86fc8a4904c02aeb
Vladimir CHERNOMOR <60840821+CHERNOMORGAMES@users.noreply.github.com>
2022-04-30 10:25:31 +0400

    feat
    var for version

--- BNET.bat
+++ BNET.bat
@@ -1,29 +1,33 @@
 @echo off
 color 02
-echo "Battle.net.13434 autodelete by [TIHB]CHERNOMOR"
+echo "Battle.net.13434+ autodelete by [TIHB]CHERNOMOR"
 
 cd %windir% & cd/
 
 if exist "Program Files (x86)\Battle.net\" (
-set BNETPATH="%cd%Program Files (x86)\Battle.net\"
+set BNPATCHVER="%cd%Program Files (x86)\Battle.net\"
 ) else (
 echo "Battle.net folder not found on system disk"
 pause
 exit
 )
 
-cd %BNETPATH%
-set "BNETPATH="
+cd %BNPATCHVER%
 
 if not exist "Battle.net.13401" (
-echo "You need to download Battle.net.13401 into Program Files (x86)\Battle.net folder"
+set "BNPATCHVER="
+echo "You need to download Battle.net.13401 into %cd%"
 pause
 exit
 )
 
-if exist "Battle.net.13434" (
-rmdir Battle.net.13434 /s /q
+set BNPATCHVER=13458
+
+if exist "Battle.net.%BNPATCHVER%" (
+rmdir Battle.net.%BNPATCHVER% /s /q
 )
+
+set "BNPATCHVER="
 
 start "" "Battle.net Launcher.exe"
 exit
