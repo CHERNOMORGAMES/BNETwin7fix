ea4c62fe16844117d895206f346db8d8b91becf6
Vladimir CHERNOMOR <60840821+CHERNOMORGAMES@users.noreply.github.com>
2022-04-30 10:20:23 +0400

    upd

--- BNET.bat
+++ BNET.bat
@@ -1,23 +1,29 @@
 @echo off
 color 02
-echo “Battle.net.13434 autodelete by [TIHB]CHERNOMOR”
+echo "Battle.net.13434 autodelete by [TIHB]CHERNOMOR"
 
 cd %windir% & cd/
 
-if exist “Program Files (x86)\Battle.net” (
-set BNETPATH="%cd%Program Files (x86)\Battle.net"
+if exist "Program Files (x86)\Battle.net\" (
+set BNETPATH="%cd%Program Files (x86)\Battle.net\"
 ) else (
-echo “Battle.net folder not found on system disk”
+echo "Battle.net folder not found on system disk"
 pause
 exit
 )
 
 cd %BNETPATH%
+set "BNETPATH="
 
-if exist “Battle.net.13434” (
+if not exist "Battle.net.13401" (
+echo "You need to download Battle.net.13401 into Program Files (x86)\Battle.net folder"
+pause
+exit
+)
+
+if exist "Battle.net.13434" (
 rmdir Battle.net.13434 /s /q
 )
 
-set “BNETPATH=”
-start “” “Battle.net Launcher.exe”
+start "" "Battle.net Launcher.exe"
 exit
